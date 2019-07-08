87750f80f48c4b0e56d4db3f61a3e8b285c07877
dorisobor <troyamber@live.com>
2019-07-08 22:26:02 +0200

    updated components and added background image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Navbar from './components/Navbar'
+import TyperText from './components/TyperText'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
+    <Navbar/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <TyperText
+      heading={'I love'}
+      dataText={[
+        'design.', 'coding.'
+        ]}
+       />
+       <Footer/>
+
+
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component}  from 'react';
+import {CardFooter} from 'reactstrap';
+
+class Footer extends Component {
+    
+    render() {
+      return (
+
+            <div>
+            <CardFooter className="text-muted">Exampel link</CardFooter>
+            </div>
+          );
+        }
+      }
+
+export default Footer;
